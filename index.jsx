6ccd2df55c7a14c32808fbd36816e8b5439c0ca4
nikhil-001mehta <nikhil.001mhehta@gmail.com>
2018-09-14 00:26:51 +0530

    adding sass-lint

--- index.jsx
+++ index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import "./css/styles.scss";
 export default class ReactDateRangePicker extends Component {
   render() {
     return <div>Hello World!!!</div>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,59 @@
-let webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const extractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    entry: {
-        index: './index.jsx'
-    },
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/dist',
-        library: 'reactDateRangePicker',
-        libraryTarget: 'commonjs2'
-    },
-    devtool: 'source-map',
-    mode: 'development',
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
-                }
-            }
-        }]
-    },
-    plugins: [
-        new CopyWebpackPlugin([{
-            from: './package.json',
-            to: './package.json',
-            toType: 'file'
-        }])
-    ],
-    externals: {
-        'react': 'commonjs react',
-        'react-dom': 'commonjs react-dom'
-    }
-}
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  entry: {
+    index: "./index.jsx"
+  },
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/dist",
+    library: "reactDateRangePicker",
+    libraryTarget: "commonjs2"
+  },
+  devtool: "source-map",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new copyWebpackPlugin([
+      {
+        from: "./package.json",
+        to: "./package.json",
+        toType: "file"
+      },
+      {
+        from: "./README.md",
+        to: "./README.md",
+        toType: "file"
+      }
+    ]),
+    new extractTextPlugin("styles.css")
+  ],
+  externals: {
+    react: "commonjs react",
+    "react-dom": "commonjs react-dom"
+  }
+};
